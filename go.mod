7c3fa66b2759d33ec20e6afa32b8ecb880a8a057
arc2501 <architsrivastava2501@gmail.com>
2022-03-07 07:16:45 +0530

    Rearranged the template

--- go.mod
+++ go.mod
@@ -2,11 +2,8 @@ module github.com/arc2501/bnb
 
 go 1.17
 
-require github.com/go-chi/chi/v5 v5.0.7
-
 require (
-	github.com/alexedwards/scs/v2 v2.5.0 // indirect
-	github.com/justinas/nosurf v1.1.1 // indirect
-	golang.org/x/crypto v0.0.0-20220214200702-86341886e292 // indirect
-	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
+	github.com/alexedwards/scs/v2 v2.5.0
+	github.com/go-chi/chi/v5 v5.0.7
+	github.com/justinas/nosurf v1.1.1
 )
--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -48,5 +48,20 @@ func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 	render.RenderTemplate(w, "about.page.html", &models.TemplateData{
 		StringMap: stringMap,
 	})
+}
 
+func (m *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "make-reservation.page.html", &models.TemplateData{})
+}
+
+func (m *Repository) Generals(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "generals.page.html", &models.TemplateData{})
+}
+
+func (m *Repository) Majors(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "majors.page.html", &models.TemplateData{})
+}
+
+func (m *Repository) Availability(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "search-availability.html", &models.TemplateData{})
 }
